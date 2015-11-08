13a4a2cc0ce35a6b50ab2a21d9c0d4f3628fcfd8
Gareth Tan <garethwtan@gmail.com>
2015-11-08 02:08:33 -0500

    Actually begin to write tests.

--- __tests__/bottom_upwards_infinite_test.js
+++ __tests__/bottom_upwards_infinite_test.js
@@ -6,16 +6,19 @@ jest.dontMock('../src/computers/constantInfiniteComputer.js');
 jest.dontMock('../src/computers/arrayInfiniteComputer.js');
 jest.dontMock('../src/utils/binaryIndexSearch.js');
 jest.dontMock('../src/utils/infiniteHelpers.js');
+jest.dontMock('./helpers/renderHelpers.js');
 jest.dontMock('lodash.isfinite');
 jest.dontMock('lodash.isarray');
 
-var React = require('react/addons');
-var TestUtils = React.addons.TestUtils;
+var React = require('react');
+var TestUtils = require('react-addons-test-utils');
 var Infinite = require('../src/react-infinite.jsx');
 
 var renderHelpers = require('./helpers/renderHelpers');
 
 describe('The Basic Behavior of the Bottom Upwards Display', function() {
+  var shallowRenderer = TestUtils.createRenderer();
+
   it('does not throw an error when set', function() {
     expect(function() {
       TestUtils.renderIntoDocument(
@@ -28,13 +31,42 @@ describe('The Basic Behavior of the Bottom Upwards Display', function() {
     }).not.toThrow();
   });
 
-  it('renders a space-filling div when the total element height is less than the container height', function() {
-    var rootNode = <Infinite elementHeight={200}
+  it('renders a space-filling top spacer div when the total element height is less than the container height', function() {
+    var infinite = <Infinite elementHeight={100}
                              containerHeight={800}
                              className={"root-scrollable-node"}
                              displayBottomUpwards>
       {renderHelpers.divGenerator(1, 100)}
     </Infinite>;
+    shallowRenderer.render(infinite);
+
+    var rootNode = shallowRenderer.getRenderOutput();
+    expect(rootNode.props.children.props.children[0]).toEqual(
+      <div ref="topSpacer"
+           style={{
+             width: '100%',
+             height: 700
+           }}/>
+    );
+  });
+
+  it('does not render a space-filling top spacer div when the total element height begins to exceed the container height', function() {
+    var infinite = <Infinite elementHeight={100}
+                             containerHeight={800}
+                             className={"root-scrollable-node"}
+                             displayBottomUpwards>
+      {renderHelpers.divGenerator(9, 100)}
+    </Infinite>;
+    shallowRenderer.render(infinite);
+
+    var rootNode = shallowRenderer.getRenderOutput();
+    expect(rootNode.props.children.props.children[0]).toEqual(
+      <div ref="topSpacer"
+           style={{
+             width: '100%',
+             height: 0
+           }}/>
+    );
   });
 });
 
