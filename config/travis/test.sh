6ee181e67f5ecb7901dc22206be0eea28828c305
Gareth Tan <garethwtan@gmail.com>
2017-08-05 01:57:16 -0400

    Fix for cypress.

--- config/travis/test.sh
+++ config/travis/test.sh
@@ -3,19 +3,22 @@
 set -e
 
 if [ "$TEST" = 1 ]; then
-    npm install react react-dom prop-types create-react-class
+    npm install react react-dom prop-types && npm install
     npm run test
 fi;
 
 if [ "$TYPECHECK" = 1 ]; then
+    npm install flow-bin
     npm run typecheck
 fi;
 
 if [ "$LINT" = 1 ]; then
+    npm install react react-dom prop-types && npm install
     npm run lint
 fi;
 
 if [ "$BROWSER" = 1 ]; then
-    npm run develop
+    npm install -g cypress-cli http-server
+    http-server --silent &
     cypress run --record --key
 fi;
